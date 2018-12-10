92e7ff042f0cb9a4ac2e4017212e7d9290e5c01e
Steven Dieu <steven.dieu1+ext@gmail.com>
2018-12-10 19:32:32 +0100

    Commencement de l'intégration des pages de connexion + inscription + recovery

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,16 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {LoginComponent} from './login/login.component';
-import {RegisterComponent} from './register/register.component';
+import {LoginComponent} from './authentication/login/login.component';
+import {RegisterComponent} from './authentication/register/register.component';
+import {RecoveryComponent} from './authentication/recovery/recovery.component';
 
 const appRoutes: Routes = [
   {path: 'accueil', component: HomeComponent},
   {path: '', component: HomeComponent},
   {path: 'connexion', component: LoginComponent},
-  {path: 'inscription', component: RegisterComponent}
+  {path: 'inscription', component: RegisterComponent},
+  {path: 'recuperation-mot-de-passe', component: RecoveryComponent}
 ];
 
 @NgModule({
